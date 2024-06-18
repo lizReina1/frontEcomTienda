3135455a87d871a2ce42b9c449ae754948330091
Moira Genesis Lozano Arauz <moira_la@outlook.es>
2024-06-18 01:47:37 -0400

    acomodando el ícono de la vista de conraseña

--- src/modules/dashboard/components/UsuariosForm.vue
+++ src/modules/dashboard/components/UsuariosForm.vue
@@ -8,27 +8,29 @@
         <form @submit.prevent="handleSubmit">
           <div class="form-group">
             <div class="row">
-              <div class="col-sm-6">
+              <div class="col-sm-6 mb-3">
                 <label for="username"><b>Nombre de Usuario</b></label>
                 <input type="text" v-model="usuario.username" class="form-control" id="username" required />
               </div>
-              <div class="col-sm-6">
+              <div class="col-sm-6 mb-3">
                 <label for="email"><b>E-mail</b></label>
                 <input type="text" v-model="usuario.email" class="form-control" id="email" required />
               </div>
-              <div class="col-sm-6">
+              <div class="col-sm-6 mb-3">
                 <label for="password"><b>Contraseña</b></label>
                 <div class="input-group">
                   <input :type="showPassword ? 'text' : 'password'" v-model="usuario.password" class="form-control" id="password" required />
                   <div class="input-group-append">
-                    <span class="input-group-text" @click="togglePasswordVisibility">
-                      <font-awesome-icon :icon="showPassword ? 'eye' : 'eye-slash'" />
-                    </span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16" @click="togglePasswordVisibility">
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                    </svg>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+
     
           <div class="row justify-content-center">
             <div class="col-sm-4">
